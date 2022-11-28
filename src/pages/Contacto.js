8ccec57ed1ddf9598c50542984e58ece2be6ab79
MDForero <mforeroroldan@gmail.com>
2022-11-28 09:25:22 -0500

    mision y vision creadas, mapa ubicado

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import logo from "../images/logo.png"
 
 const Contacto = () => {
   return (
-    <div>Contacto</div>
+    <div>
+      <main className="preview">
+        <div className='logo'><img src={logo} width="400px" /></div>
+      </main>
+      <section className='container'>
+
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d993.8407094659531!2d-72.89727296941126!3d4.878750145799816!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e6ad27f9093a389%3A0xec9408b739401bec!2sIps%20Salud%20Laboral%20Monterrey!5e0!3m2!1ses-419!2sco!4v1669642952978!5m2!1ses-419!2sco" width="600px" height="450px"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </section>
+    </div>
   )
 }
 
--- src/pages/Nosotros.js
+++ src/pages/Nosotros.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import logo from "../images/logo.png"
+import mision1 from "../images/compressed_foto14.jpg"
+import mision2 from "../images/compressed_foto15.jpg"
+import vision1 from "../images/compressed_foto15.jpg"
+import vision2 from "../images/compressed_foto15.jpg"
 
 const Nosotros = () => {
   return (
     <div>
-      
+      <main className="preview">
+        <div className='logo'><img src={logo} width="400px" /></div>
+      </main>
+      <section className='container'>
+        <article className='about'>
+          <center> <h1>Misión</h1> </center>
+          <p style={{fontSize:"2rem"}}>Brindar servicios integrales en Salud Ocupacional al sector empresarial privado y público, creando una cultura preventiva que mitigue los riesgos ocupacionales y aumente la productividad de nuestros clientes, a través de un servicio eficiente y de calidad prestado por profesionales con alta experticia y con calidez humana, propendiendo mejorar la calidad de vida de la comunidad Casanareña, en particular</p>
+          <div style={{ display: "flex" }}>
+            <figure>
+              <img src={mision1} alt="" width="200px" />
+            </figure>
+            <figure>
+              <img src={mision2} alt="" width="200px" />
+            </figure>
+          </div>
+        </article>
+
+        <article className='about'>
+          <center> <h1>Visión</h1> </center>
+          <p>La IPS SALUD LABORAL MONTERREY se posicionará como empresa líder en el sur de Casanare en la prestación de servicios de Salud Ocupacional, siendo referentes en la región en la realización de exámenes médicos y paraclínicos laborales, logrando ser reconocida por su <strong>calidad y excelencia operativa</strong> y respondiendo a las necesidades del cliente y a la normatividad respectiva.</p>
+          <div style={{ display: "flex" }}>
+            <figure>
+              <img src={vision1} alt="" width="200px" />
+            </figure>
+            <figure>
+              <img src={vision2} alt="" width="200px" />
+            </figure>
+          </div>
+        </article>
+      </section>
     </div>
   )
 }
