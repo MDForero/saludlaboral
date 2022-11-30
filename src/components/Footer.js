412460714061de0e75efab70d2a382f99bced6d7
MDForero <mforeroroldan@gmail.com>
2022-11-30 16:46:20 -0500

    whatsapp

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
                 <h4>👇!Síguenos en nuestras redes¡👇</h4>
                 <ul className="redes">
                     <li><a href='https://www.facebook.com/saludlaboralmonterrey' target="_blank" className='nav-link'><FontAwesomeIcon icon={faFacebook} size="2x" /></a></li>
-                    <li><a href='wa.me/+573219251480' className='nav-link' target="_blank"><FontAwesomeIcon icon={faWhatsapp} target="_blank" size="2x" /></a></li>
+                    <li><a href='https://wa.me/+573112038078' className='nav-link' target="_blank"><FontAwesomeIcon icon={faWhatsapp} target="_blank" size="2x" /></a></li>
                 </ul>
             </div>
             <div>
